1d2214682d7c0d4075d04c9afa8b40cbcf153fd2
GiladFisher <giladfisher98@gmail.com>
2022-12-21 12:47:58 +0200

    first

--- src/builder/ConcreteMember.java
+++ src/builder/ConcreteMember.java
@@ -1,10 +1,14 @@
 package builder;
 
 public class ConcreteMember implements Member{
+    UndoableStringBuilder usb;
+    String name;
+    GroupAdmin group;
     @Override
     public void update(UndoableStringBuilder usb) {
         // TODO Auto-generated method stub
 
     }
+
 }
 
--- src/builder/GroupAdmin.java
+++ src/builder/GroupAdmin.java
@@ -1,18 +1,29 @@
 package builder;
 
+import java.util.ArrayList;
+
 public class GroupAdmin implements Sender{
+    ArrayList<Member> members = new ArrayList<Member>();
+    UndoableStringBuilder usb = new UndoableStringBuilder();
+
     @Override
     public void register(Member obj) {
+        // TODO Auto-generated method stub
+        members.add(obj);
         
     }
 
     @Override
     public void unregister(Member obj) {
-
+        // TODO Auto-generated method stub
+        members.remove(obj);
     }
 
     @Override
     public void insert(int offset, String obj) {
+// TODO Auto-generated method stub
+        usb.insert(offset, obj);
+
 
     }
 
